cc40f4e16f4b6f294b8eb593ae9d5116979a9e62
Gasser <gaserkhaled@yahoo.com>
2020-08-20 21:39:41 -0400

    heroku bugs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,8 @@
-const AppError = require('../utils/appError');
+const appError = require('../utils/appError');
 
 const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
-  return new AppError(message, 400);
+  return new appError(message, 400);
 };
 
 const handleDuplicateFieldsDB = err => {
@@ -10,21 +10,21 @@ const handleDuplicateFieldsDB = err => {
   console.log(value);
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
-  return new AppError(message, 400);
+  return new appError(message, 400);
 };
 
 const handleValidationErrorDB = err => {
   const errors = Object.values(err.errors).map(el => el.message);
 
   const message = `Invalid input data. ${errors.join('. ')}`;
-  return new AppError(message, 400);
+  return new appError(message, 400);
 };
 
 const handleJWTError = () =>
-  new AppError('Invalid token. Please log in again!', 401);
+  new appError('Invalid token. Please log in again!', 401);
 
 const handleJWTExpiredError = () =>
-  new AppError('Your token has expired! Please log in again.', 401);
+  new appError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, req, res) => {
   if (req.originalUrl.startsWith('/api')) {
